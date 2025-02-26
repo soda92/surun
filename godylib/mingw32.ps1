e9d2fa4622ddc8bcfce81e71301c94ede523ce83
sodacris <wjxa20152015@gmail.com>
2025-02-27 01:46:37 +0800

    fix bash

--- godylib/mingw32.ps1
+++ godylib/mingw32.ps1
@@ -1,5 +1,6 @@
 $Env:MSYSTEM = "MINGW32"
 $prof = $Env:USERPROFILE
 $shell = "bash.exe"
+$Env:CHERE_INVOKING=1
 
-& "$prof/scoop/apps/msys2/2024-12-08/usr/bin/$shell" -c $args[0]
+& "$prof/scoop/apps/msys2/2024-12-08/usr/bin/$shell" -l $args[0]
