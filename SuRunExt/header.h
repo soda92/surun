62af0af30e78b02f12acfe2308b2d85eacdbc348
sodacris <wjxa20152015@gmail.com>
2025-03-03 12:12:46 +0800

    minor fix

--- SuRunExt/header.h
+++ SuRunExt/header.h
@@ -3,13 +3,13 @@
 #include <windows.h>
 #include <winwlx.h>
 
+extern "C" {
 #define DllExport __declspec(dllexport)
 
-extern "C" {
-LONG CALLBACK DllExport CPlApplet(HWND hwnd, UINT uMsg,
-                                              LPARAM lParam1, LPARAM lParam2);
+LONG CALLBACK DllExport CPlApplet(HWND hwnd, UINT uMsg, LPARAM lParam1,
+                                  LPARAM lParam2);
 STDAPI DllExport DllCanUnloadNow(void);
 STDAPI DllExport DllGetClassObject(REFCLSID rclsid, REFIID riid,
-                                               LPVOID *ppvOut);
+                                   LPVOID *ppvOut);
 VOID APIENTRY DllExport SuRunLogoffUser(PWLX_NOTIFICATION_INFO Info);
 }
