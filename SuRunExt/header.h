2ed614649a21e7c10fc1721a3eb4e7389d1d1611
sodacris <wjxa20152015@gmail.com>
2025-03-03 11:42:11 +0800

    try add dllexport

--- SuRunExt/header.h
+++ SuRunExt/header.h
@@ -3,9 +3,13 @@
 #include <windows.h>
 #include <winwlx.h>
 
+#define DllExport __declspec(dllexport)
+
 extern "C" {
-LONG CALLBACK CPlApplet(HWND hwnd, UINT uMsg, LPARAM lParam1, LPARAM lParam2);
-STDAPI DllCanUnloadNow(void);
-STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppvOut);
-VOID APIENTRY SuRunLogoffUser(PWLX_NOTIFICATION_INFO Info);
+LONG CALLBACK DllExport CPlApplet(HWND hwnd, UINT uMsg,
+                                              LPARAM lParam1, LPARAM lParam2);
+STDAPI DllExport DllCanUnloadNow(void);
+STDAPI DllExport DllGetClassObject(REFCLSID rclsid, REFIID riid,
+                                               LPVOID *ppvOut);
+VOID APIENTRY DllExport SuRunLogoffUser(PWLX_NOTIFICATION_INFO Info);
 }
