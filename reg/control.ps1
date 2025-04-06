dfadaddba58908b0283dbed126211e4be3b5692a
sodacris <wjxa20152015@gmail.com>
2025-04-07 02:52:13 +0800

    add reg

---
+++ reg/control.ps1
@@ -0,0 +1,3 @@
+"Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel\NameSpace"
+
+regjump "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Control Panel\\Extended Properties\{305CA226-D286-468e-B848-2B2E8E697B74}"
