a95ce5a194ecbacf24583ac028056c4d25c2ab62
Maple <wjxa20152015@gmail.com>
2025-05-01 02:44:59 +0800

    fix build script
    now we only to build two passes. the install surun was built manually without dependencies, so make sure build 32 and 64 bit version of both surun and surunext first, see the build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -16,9 +16,11 @@ Write-Host "$name amd64 Command Prompt variables set." -ForegroundColor Green
 
 $root = Split-Path -Path $PSScriptRoot -Parent
 $surun = Join-Path -Path $root -ChildPath "PC/SuRunVC9.sln"
+$install_surun = Join-Path -Path $root -ChildPath "PC/InstallSuRunVC9.vcxproj"
 
 # for faster build, comment the next line and change "/t:Rebuild" to "/t:Build"
 # & "msbuild.exe" $surun /t:clean 1>NUL 2>NUL
 & "msbuild.exe" $surun /t:Build /p:Configuration="x64 Unicode Debug" /p:Platform=x64
 & "msbuild.exe" $surun /t:Build /p:Configuration="SuRun32 Unicode Debug" /p:Platform=Win32
-& "msbuild.exe" $surun /t:Build /p:Configuration="Unicode Debug" /p:Platform=Win32
+
+& "msbuild.exe" $install_surun /t:Build /p:Configuration="Debug" /p:Platform=Win32
