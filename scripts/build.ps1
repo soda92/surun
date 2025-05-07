30f9b020c126079ff71df1fafb0d37a999da1ac1
Maple <wjxa20152015@gmail.com>
2025-05-07 20:32:23 +0800

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -5,12 +5,11 @@ $name = & $vswherePath -products * -requires Microsoft.VisualStudio.Component.VC
 Push-Location "$vs\VC\Auxiliary\Build"
 
 cmd /c "vcvars64.bat & set" |
-	ForEach-Object {
-		if ($_ -match "=")
-		{
-			$v = $_.split("=", 2); Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
-		}
+ForEach-Object {
+	if ($_ -match "=") {
+		$v = $_.split("=", 2); Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
 	}
+}
 Pop-Location
 Write-Host "$name amd64 Command Prompt variables set." -ForegroundColor Green
 
@@ -18,10 +17,27 @@ $root = Split-Path -Path $PSScriptRoot -Parent
 $surun = Join-Path -Path $root -ChildPath "src/PC/SuRun.sln"
 $install_surun = Join-Path -Path $root -ChildPath "src/PC/InstallSuRun.vcxproj"
 
+function Try_Exec {
+	param(
+		[Parameter(Mandatory = $true)]
+		[string]$CommandToExecute
+	)
+
+	Write-Host "Executing: $($CommandToExecute)"
+	Invoke-Expression $CommandToExecute
+
+	if ($LastExitCode -ne 0) {
+		Write-Error "Command failed with exit code: $LastExitCode"
+		Write-Error $command
+		exit $LastExitCode
+	}
+}
+
 # for faster build, comment the next line and change "/t:Rebuild" to "/t:Build"
 & "msbuild.exe" $surun /t:clean 1>NUL 2>NUL
-& "msbuild.exe" $surun /t:Build /p:Configuration="x64 Unicode Debug" /p:Platform=x64
-& "msbuild.exe" $surun /t:Build /p:Configuration="SuRun32 Unicode Debug" /p:Platform=Win32
+Try_Exec "msbuild.exe $surun /t:Build /p:Configuration=`"x64 Unicode Debug`" /p:Platform=x64"
+
+Try_Exec "msbuild.exe $surun /t:Build /p:Configuration=`"SuRun32 Unicode Debug`" /p:Platform=Win32"
 
 & "msbuild.exe" $install_surun /t:clean 1>NUL 2>NUL
-& "msbuild.exe" $install_surun /t:Build /p:Configuration="Debug" /p:Platform=Win32
+Try_Exec "msbuild.exe $install_surun /t:Build /p:Configuration=`"Debug`" /p:Platform=Win32"
